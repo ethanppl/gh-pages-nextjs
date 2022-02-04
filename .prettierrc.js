3f996d70ec5e88a1c911f7ae31440ce3a1b7aa64
Ethan <43922849+ethanppl@users.noreply.github.com>
2022-02-04 14:28:30 +0800

    image, base path, prettier & type check

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false,
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const ON_GITHUB_PAGES = process.env.NODE_ENV === 'production';
+
+// replace this with your repository name
+const repoName = 'gh-pages-nextjs';
+
+const basePath = ON_GITHUB_PAGES ? `/${repoName}` : '';
+const assetPrefix = ON_GITHUB_PAGES ? `/${repoName}/` : '';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath,
+  assetPrefix,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
