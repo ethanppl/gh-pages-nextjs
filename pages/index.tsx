9d1e0c456e10d71efbdc426a825565bc764ea530
Ethan <43922849+ethanppl@users.noreply.github.com>
2022-02-23 11:04:11 +0800

    fix favicon link (#6)

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
           name="description"
           content="This is a Next.js template that can be exported as a static website and deployed on GitHub Pages."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
 
       <main className={styles.main}>
